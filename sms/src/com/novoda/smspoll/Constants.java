4c1c03a148ee7ab093b65d54d4aad2ae0d570417
kevinmcdonagh <kevin@appletv.co.uk>
2009-07-28 19:44:00 +0100

    Updated the README with the icon copyright details. Also finished adding the add contact button the sms app. Then formatted the code.

--- sms/src/com/novoda/smspoll/Constants.java
+++ sms/src/com/novoda/smspoll/Constants.java
@@ -1,9 +1,14 @@
 package com.novoda.smspoll;
 
+import android.provider.Contacts;
+
 public class Constants {
-	public static final String	SENT						= "SMS_SENT";
-	public static final String	DELIVERED					= "SMS_DELIVERED";
-	public static final int		PICK_POLL					= 555;
-	public static final String	INTENT_DATA_SMS_CONTENTS	= "SMS_DATA";
-	public static final int		PICK_POLL_REQUEST			= 2345678;
+	public static final String		SENT						= "SMS_SENT";
+	public static final String		DELIVERED					= "SMS_DELIVERED";
+	public static final int			PICK_POLL					= 555;
+	public static final String		INTENT_DATA_SMS_CONTENTS	= "SMS_DATA";
+	public static final int			PICK_POLL_REQUEST			= 2345678;
+	public static final int			PICK_CONTACT				= 1;
+	public static final String[]	PEOPLE_PROJECTION			= new String[] { Contacts.People._ID, Contacts.People.PRIMARY_PHONE_ID, Contacts.People.TYPE,
+			Contacts.People.NUMBER, Contacts.People.LABEL, Contacts.People.NAME, };
 }
--- sms/src/com/novoda/smspoll/SMS.java
+++ sms/src/com/novoda/smspoll/SMS.java
@@ -1,17 +1,15 @@
 package com.novoda.smspoll;
 
-
-import org.w3c.dom.Text;
-
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Contacts;
+import android.provider.Contacts.People;
 import android.telephony.gsm.SmsManager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,15 +23,18 @@ import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.Filterable;
+import android.widget.ImageButton;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SMS extends Activity {
-	Button		btnSendSMS;
-	EditText	txtPhoneNo;
-	EditText	txtMessage;
-	private Menu	mOptMenu; 	
+	private static final String	TAG	= null;
+	Button						btnSendSMS;
+	ImageButton					btnAddContacts;
+	EditText					txtPhoneNo;
+	EditText					txtMessage;
+	private Menu				mOptMenu;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -43,77 +44,69 @@ public class SMS extends Activity {
 		btnSendSMS = (Button) findViewById(R.id.btnSendSMS);
 		txtPhoneNo = (EditText) findViewById(R.id.txtPhoneNo);
 		txtMessage = (EditText) findViewById(R.id.txtMessage);
+		btnAddContacts = (ImageButton) findViewById(R.id.addContact);
 
-		btnSendSMS.setOnClickListener(getNewSendSmsListener()); 
-		
-	    Cursor peopleCursor = 	getContentResolver().query(Contacts.People.CONTENT_URI, PEOPLE_PROJECTION, null, null, Contacts.People.DEFAULT_SORT_ORDER);
-	    ContactListAdapter contactadapter = new	ContactListAdapter(this,peopleCursor);
-	    
+		btnSendSMS.setOnClickListener(getNewSendSmsListener());
+		btnAddContacts.setOnClickListener(getNewAddContactsListener());
+
+		Cursor peopleCursor = getContentResolver().query(Contacts.People.CONTENT_URI, Constants.PEOPLE_PROJECTION, null, null, Contacts.People.DEFAULT_SORT_ORDER);
+		ContactListAdapter contactadapter = new ContactListAdapter(this, peopleCursor);
+
 		MultiAutoCompleteTextView textView = (MultiAutoCompleteTextView) findViewById(R.id.txtPhoneNo);
-        textView.setAdapter(contactadapter);
-        textView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
-    }
+		textView.setAdapter(contactadapter);
+		textView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+	}
 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		mOptMenu = menu;
 
-    public static class ContactListAdapter extends CursorAdapter implements Filterable {
-        public ContactListAdapter(Context context, Cursor c) {
-            super(context, c);
-            mContent = context.getContentResolver();
-        }
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.sms_menu_actions, menu);
+		return true;
+	}
 
-        @Override
-        public View newView(Context context, Cursor cursor, ViewGroup parent) {
-            final LayoutInflater inflater = LayoutInflater.from(context);
-            final TextView view = (TextView) inflater.inflate(
-                    android.R.layout.simple_dropdown_item_1line, parent, false);
-            view.setText(cursor.getString(5));
-            return view;
-        }
+	public boolean onOptionsItemSelected(MenuItem item) {
 
-        @Override
-        public void bindView(View view, Context context, Cursor cursor) {
-            ((TextView) view).setText(cursor.getString(5));
-        }
+		Intent intent = new Intent();
+		switch (item.getItemId()) {
+			case R.id.attachpoll:
+				Log.v(TAG, "User selected to add a new Poll");
+				intent.setClassName(getBaseContext(), "com.novoda.smspoll.Poll");
+				startActivityForResult(intent, Constants.PICK_POLL_REQUEST);
+				return true;
+		}
+		return false;
+	}
 
-        @Override
-        public String convertToString(Cursor cursor) {
-            return cursor.getString(5);
-        }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == Constants.PICK_POLL_REQUEST && resultCode == RESULT_OK) {
+			Log.v(TAG, "A message has been intercepted with contents[" + intent.getStringExtra(Constants.INTENT_DATA_SMS_CONTENTS) + "]");
+			Log.d(TAG, "Intent URI[" + intent.toURI() + "]");
+			txtMessage.setText(intent.getStringExtra(Constants.INTENT_DATA_SMS_CONTENTS));
+			txtMessage.invalidate();
+		}
 
-        @Override
-        public Cursor runQueryOnBackgroundThread(CharSequence constraint) {
-            if (getFilterQueryProvider() != null) {
-                return getFilterQueryProvider().runQuery(constraint);
-            }
+		if (requestCode == Constants.PICK_CONTACT && resultCode == RESULT_OK) {
+			Uri contactData = intent.getData();
+			Cursor c = managedQuery(contactData, null, null, null, null);
+			if (c.moveToFirst()) {
+				String name = c.getString(c.getColumnIndexOrThrow(People.NAME));
+				String currentContents = txtPhoneNo.getText().toString();
+				txtPhoneNo.setText(currentContents + "," + name + ",");
+			}
+		}
 
-            StringBuilder buffer = null;
-            String[] args = null;
-            if (constraint != null) {
-                buffer = new StringBuilder();
-                buffer.append("UPPER(");
-                buffer.append(Contacts.ContactMethods.NAME);
-                buffer.append(") GLOB ?");
-                args = new String[] { constraint.toString().toUpperCase() + "*" };
-            }
+	}
 
-            return mContent.query(Contacts.People.CONTENT_URI, PEOPLE_PROJECTION,
-                    buffer == null ? null : buffer.toString(), args,
-                    Contacts.People.DEFAULT_SORT_ORDER);
-        }
-
-        private ContentResolver mContent;        
-    }
-
-    private static final String[] PEOPLE_PROJECTION = new String[] {
-        Contacts.People._ID,
-        Contacts.People.PRIMARY_PHONE_ID,
-        Contacts.People.TYPE,
-        Contacts.People.NUMBER,
-        Contacts.People.LABEL,
-        Contacts.People.NAME,
-    };
-	private static final String	TAG	= null;
-    
+	private OnClickListener getNewAddContactsListener() {
+		return new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_PICK, People.CONTENT_URI);
+				startActivityForResult(intent, Constants.PICK_CONTACT);
+			}
+		};
+	}
 
 	private OnClickListener getNewSendSmsListener() {
 		return new View.OnClickListener() {
@@ -128,35 +121,51 @@ public class SMS extends Activity {
 			}
 		};
 	}
-	
-	public boolean onCreateOptionsMenu(Menu menu) {
-		mOptMenu = menu;
 
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.sms_menu_actions, menu);
-		return true;
-	}
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public static class ContactListAdapter extends CursorAdapter implements Filterable {
+		public ContactListAdapter(Context context, Cursor c) {
+			super(context, c);
+			mContent = context.getContentResolver();
+		}
 
-		Intent intent = new Intent();
-		switch (item.getItemId()) {
-			case R.id.attachpoll:
-				Log.v(TAG, "User selected to add a new Poll");
-				intent.setClassName(getBaseContext(), "com.novoda.smspoll.Poll");
-				startActivityForResult(intent, Constants.PICK_POLL_REQUEST);
-				return true;
+		@Override
+		public View newView(Context context, Cursor cursor, ViewGroup parent) {
+			final LayoutInflater inflater = LayoutInflater.from(context);
+			final TextView view = (TextView) inflater.inflate(android.R.layout.simple_dropdown_item_1line, parent, false);
+			view.setText(cursor.getString(5));
+			return view;
 		}
-		return false;
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (requestCode == Constants.PICK_POLL_REQUEST && resultCode == RESULT_OK) {
-			Log.v(TAG, "A message has been intercepted with contents[" + intent.getStringExtra(Constants.INTENT_DATA_SMS_CONTENTS) + "]");
-			Log.d(TAG, "Intent URI[" + intent.toURI() + "]");
-			txtMessage.setText(intent.getStringExtra(Constants.INTENT_DATA_SMS_CONTENTS));
-			txtMessage.invalidate();
+
+		@Override
+		public void bindView(View view, Context context, Cursor cursor) {
+			((TextView) view).setText(cursor.getString(5));
 		}
+
+		@Override
+		public String convertToString(Cursor cursor) {
+			return cursor.getString(5);
+		}
+
+		@Override
+		public Cursor runQueryOnBackgroundThread(CharSequence constraint) {
+			if (getFilterQueryProvider() != null) {
+				return getFilterQueryProvider().runQuery(constraint);
+			}
+
+			StringBuilder buffer = null;
+			String[] args = null;
+			if (constraint != null) {
+				buffer = new StringBuilder();
+				buffer.append("UPPER(");
+				buffer.append(Contacts.ContactMethods.NAME);
+				buffer.append(") GLOB ?");
+				args = new String[] { constraint.toString().toUpperCase() + "*" };
+			}
+
+			return mContent.query(Contacts.People.CONTENT_URI, Constants.PEOPLE_PROJECTION, buffer == null ? null : buffer.toString(), args, Contacts.People.DEFAULT_SORT_ORDER);
+		}
+
+		private ContentResolver	mContent;
 	}
 
 	private boolean pollIsValid(String phoneNo, String message) {
@@ -164,12 +173,14 @@ public class SMS extends Activity {
 	}
 
 	private void sendSMS(String phoneNumber, String message) {
-//		registerReceiver(new SmsSentReciever(), new IntentFilter(Constants.SENT));
-//		registerReceiver(new SmsDeliveredReciever(), new IntentFilter(Constants.DELIVERED));
+		// registerReceiver(new SmsSentReciever(), new
+		// IntentFilter(Constants.SENT));
+		// registerReceiver(new SmsDeliveredReciever(), new
+		// IntentFilter(Constants.DELIVERED));
 
 		PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(Constants.SENT), 0);
 		PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(Constants.DELIVERED), 0);
-		
+
 		SmsManager.getDefault().sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
 	}
 
