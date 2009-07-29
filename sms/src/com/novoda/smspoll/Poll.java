fe3825a62952ec3247f9703949b13b3e926a0d30
kevinmcdonagh <kevin@appletv.co.uk>
2009-07-29 15:43:52 +0100

    Removed the black BG from row items, added null checks, added deletion of poll items.

--- sms/src/com/novoda/smspoll/Poll.java
+++ sms/src/com/novoda/smspoll/Poll.java
@@ -8,18 +8,29 @@ import java.util.Map;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class Poll extends ListActivity {
 	private static final String					INTENT_DATA_OPTION			= "option1";
@@ -28,12 +39,15 @@ public class Poll extends ListActivity {
 	private static final String					ANSWER_KEY					= "answer";
 	protected static final String				TAG							= "[Poll]";
 	private ArrayList<HashMap<String, String>>	list						= new ArrayList<HashMap<String, String>>();
-	private SimpleAdapter						adapter;
+	private SimpleAdapter						answerAdapter;
 	private EditText							btn_newAnswer;
 	private EditText							btn_question;
 	private ImageButton							btn_addAnswer;
 	private Button								btn_accept;
 	private ArrayList<String>					answers;
+	private int									mChosenPosition;
+	protected Object	mNewFileName;
+	private Object	sCurrFileName;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -45,18 +59,50 @@ public class Poll extends ListActivity {
 		this.btn_newAnswer = (EditText) findViewById(R.id.answer);
 		this.btn_accept = (Button) findViewById(R.id.btnAcceptPoll);
 
-		this.adapter = new SimpleAdapter(this, list, R.layout.row_answer, new String[] { ANSWER_KEY }, new int[] { R.id.answer });
-		setListAdapter(this.adapter);
+		this.answerAdapter = new SimpleAdapter(this, list, R.layout.row_answer, new String[] { ANSWER_KEY }, new int[] { R.id.answer });
+		setListAdapter(this.answerAdapter);
 
 		this.btn_question.setOnClickListener(getQuestionOnClickListener());
 		this.btn_addAnswer.setOnClickListener(getAnswerAddBtnClickListener());
 		this.btn_newAnswer.setOnClickListener(getNewAnswerOnCLickListener());
-
 		this.btn_accept.setOnClickListener(getAcceptBtnOnClickListener());
+
+		setListLongClicks();
 
 		addAnswer("Two");
 		addAnswer("Twenty");
 
+	}
+
+	private void setListLongClicks() {
+		ListView lv = getListView();
+
+		lv.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+			public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+				MenuInflater inflater = getMenuInflater();
+				inflater.inflate(R.menu.poll_item_actions, menu);
+			}
+		});
+
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				mChosenPosition = position;
+				getListView().showContextMenu();
+			}
+		});
+
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.delete:
+				removeAnswer(mChosenPosition);
+				return true;
+		}
+
+		return true;
 	}
 
 	private OnClickListener getAcceptBtnOnClickListener() {
@@ -68,19 +114,19 @@ public class Poll extends ListActivity {
 				intent.putExtra(INTENT_DATA_QUESTION, question);
 				intent.putExtra(INTENT_DATA_TOTAL_ANSWERS, answers_total);
 				answers = new ArrayList<String>();
-				answers.add(0,"1");
-				answers.add(1,"20");
-				answers.add(2,"40");
-				
+				answers.add(0, "1");
+				answers.add(1, "20");
+				answers.add(2, "40");
+
 				String smsTxt = question + "     ";
 				int answerNumber;
-				for(int i=0; i<answers_total;i++){
-					answerNumber = i+1;
+				for (int i = 0; i < answers_total; i++) {
+					answerNumber = i + 1;
 					smsTxt = smsTxt + "\n" + answerNumber + "." + answers.get(i);
 				}
-				
+
 				smsTxt = smsTxt + "\nVote: # <option> ie. #1 or #2";
-				
+
 				intent.putExtra(Constants.INTENT_DATA_SMS_CONTENTS, smsTxt);
 				setResult(RESULT_OK, intent);
 				finish();
@@ -116,7 +162,7 @@ public class Poll extends ListActivity {
 	private OnClickListener getNewAnswerOnCLickListener() {
 		return new OnClickListener() {
 			public void onClick(View v) {
-				
+
 			}
 		};
 	}
@@ -133,11 +179,15 @@ public class Poll extends ListActivity {
 		return new OnClickListener() {
 			public void onClick(View view) {
 				try {
-					addAnswer();
-					adapter.notifyDataSetChanged();
-					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-					imm.hideSoftInputFromWindow(btn_newAnswer.getWindowToken(), 0);
-					btn_newAnswer.setText("");
+
+					if (btn_newAnswer.getText().toString().length() >= 1) {
+						Log.i(TAG, "text contains : " + btn_newAnswer.getText().toString());
+						addAnswer();
+						InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+						imm.hideSoftInputFromWindow(btn_newAnswer.getWindowToken(), 0);
+						btn_newAnswer.setText("");
+					}
+
 				} catch (NullPointerException e) {
 					Log.v(TAG, "Tried to add null value");
 				}
@@ -149,12 +199,20 @@ public class Poll extends ListActivity {
 		HashMap<String, String> item = new HashMap<String, String>();
 		item.put(ANSWER_KEY, btn_newAnswer.getText().toString());
 		list.add(item);
+		answerAdapter.notifyDataSetChanged();
 	}
 
 	private void addAnswer(String answer) {
 		HashMap<String, String> item = new HashMap<String, String>();
 		item.put(ANSWER_KEY, answer);
 		list.add(item);
+		answerAdapter.notifyDataSetChanged();
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void removeAnswer(int itemPosition) {
+		list.remove((HashMap<String, String>) answerAdapter.getItem(itemPosition));
+		answerAdapter.notifyDataSetChanged();
 	}
 
 }
